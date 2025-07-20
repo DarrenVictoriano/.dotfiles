6a353d3b2b217b179887a3ee216a61a7c549ee88
Darren Victoriano <darren.victoriano@gmail.com>
2025-07-20 12:51:39 -0700

    updated outline to not fold (#55)
    * updated outline to not fold
    
    * updated outline to not fold

--- nvim/.config/nvim/lua/darren/plugins/outline.lua
+++ nvim/.config/nvim/lua/darren/plugins/outline.lua
@@ -17,4 +17,26 @@ return {
 			width = 33,
 		},
 	},
+	config = function(_, opts)
+		require("outline").setup(opts)
+
+		-- Disable folding in the outline window
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "Outline",
+			callback = function()
+				vim.wo.foldenable = false
+				-- vim.wo.foldlevel = 99
+			end,
+		})
+
+		-- Force open all sysmbol folds after outline opens
+		-- vim.api.nvim_create_autocmd("User", {
+		-- 	pattern = "OutlineOpen",
+		-- 	callback = function()
+		-- 		vim.defer_fn(function()
+		-- 			vim.cmd.normal("zR")
+		-- 		end, 50)
+		-- 	end,
+		-- })
+	end,
 }
