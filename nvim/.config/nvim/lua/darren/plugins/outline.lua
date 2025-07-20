c5ee1d80b450005e2a522f002a6df3a1391d8f0a
Darren Victoriano <darren.victoriano@gmail.com>
2025-07-20 13:27:42 -0700

    auto close outline

--- nvim/.config/nvim/lua/darren/plugins/outline.lua
+++ nvim/.config/nvim/lua/darren/plugins/outline.lua
@@ -29,6 +29,26 @@ return {
 			end,
 		})
 
+		-- Automatically close the Outline window when cursor leaves it
+		vim.api.nvim_create_autocmd("WinLeave", {
+			pattern = "*",
+			callback = function(args)
+				local buftype = vim.api.nvim_buf_get_option(args.buf, "filetype")
+				if buftype == "Outline" then
+					-- Delay to avoid interrupting the jump to the target symbol
+					vim.defer_fn(function()
+						local wins = vim.api.nvim_list_wins()
+						for _, win in ipairs(wins) do
+							local buf = vim.api.nvim_win_get_buf(win)
+							if vim.api.nvim_buf_get_option(buf, "filetype") == "Outline" then
+								vim.api.nvim_win_close(win, true)
+							end
+						end
+					end, 100)
+				end
+			end,
+		})
+
 		-- Force open all sysmbol folds after outline opens
 		-- vim.api.nvim_create_autocmd("User", {
 		-- 	pattern = "OutlineOpen",
