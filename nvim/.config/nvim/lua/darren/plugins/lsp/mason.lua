81b2a25a19176bfaa5e5eaf6f1c5d52ac2d35840
Darren Victoriano <darren.victoriano@dexcom.com>
2025-08-12 08:53:48 -0700

    tokyo night

--- nvim/.config/nvim/lua/darren/plugins/lsp/mason.lua
+++ nvim/.config/nvim/lua/darren/plugins/lsp/mason.lua
@@ -1,66 +1,53 @@
 return {
-	"williamboman/mason.nvim",
-	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-	},
-	config = function()
-		-- import mason
-		local mason = require("mason")
-
-		-- import mason-lspconfig
-		local mason_lspconfig = require("mason-lspconfig")
-		local mason_tool_installer = require("mason-tool-installer")
-
-		-- enable mason and configure icons
-		mason.setup({
-			ui = {
-				icons = {
-					package_installed = "✓",
-					package_pending = "➜",
-					package_uninstalled = "✗",
-				},
-			},
-		})
-
-		-- NOTE: to install using the ui
-		-- command is :Mason
-		-- i for install
-		-- X to uninstall
-		mason_lspconfig.setup({
-			-- list of servers for mason to install
-			ensure_installed = {
-				"ts_ls",
-				"html",
-				"cssls",
-				"lua_ls",
-				"pyright",
-				"clangd",
-                "rust_analyzer",
-				"robotframework_ls",
-				"bashls", -- requires rust
-				-- "omnisharp",
-				"marksman",
-				"yamlls",
-				-- "csharp_ls",
-				-- "gdscript",
-				-- "gopls",
-			},
-		})
-
-		mason_tool_installer.setup({
-			ensure_installed = {
-				"prettier", -- prettier formatter
-				"stylua", -- lua formatter
-				"ruff", -- python linter/formatter (fast, coded in Rust)
-				"shellharden", -- bash linter
-				-- "isort", -- python formatter
-				-- "black", -- python formatter
-				-- "pylint", -- python linter
-				-- "mypy", -- python linter
-				-- "eslint_d", -- js linter
-				-- "ast-grep", -- csharp linter/formatter
-			},
-		})
-	end,
+  "williamboman/mason.nvim",
+  priority = 1000, -- ensure Mason loads before dependent plugins
+  dependencies = {
+    {
+      "williamboman/mason-lspconfig.nvim",
+      config = function()
+        require("mason-lspconfig").setup({
+          ensure_installed = {  -- LSPs
+            "ts_ls",
+            "html",
+            "cssls",
+            "lua_ls",
+            "pyright",
+            "clangd",
+            "rust_analyzer",
+            "robotframework_ls",
+            "bashls",
+            "marksman",
+            "yamlls",
+          },
+          automatic_installation = true,
+          -- automatic_setup = false, -- optional: disable auto-setup if using setup_handlers
+        })
+      end,
+    },
+    {
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
+      config = function()
+        require("mason-tool-installer").setup({
+          ensure_installed = {  -- Formatters
+            "prettier",
+            "stylua",
+            "ruff",
+            "shellharden",
+          },
+        })
+      end,
+    },
+  },
+  config = function()
+    require("mason").setup({
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
+    })
+  end,
 }
+
